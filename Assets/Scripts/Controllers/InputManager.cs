8f6299ced06bc3e48366846eb4c1d648fb9108be
Areg <areg06.06@mail.ru>
2025-02-13 00:23:49 +0300

    add Kick input

--- Assets/Scripts/Controllers/InputManager.cs
+++ Assets/Scripts/Controllers/InputManager.cs
@@ -12,17 +12,22 @@ namespace Controllers
         public Vector2 MousePosition => Mouse.current.position.ReadValue();
         public Vector2 MoveDirection => _moveAction.ReadValue<Vector2>();
         public Action OnAttack;
+        public Action OnKick;
         
         [Inject] private InputActionAsset _actionAsset;
         private InputAction _moveAction;
         private InputAction _attackAction;
+        private InputAction _kickAction;
         
         public void Start()
         {
             _actionAsset.Enable();
             _moveAction = _actionAsset.FindActionMap("Player").FindAction("Move");
             _attackAction = _actionAsset.FindActionMap("Player").FindAction("Attack");
+            _kickAction = _actionAsset.FindActionMap("Player").FindAction("Kick");
+            
             _attackAction.performed += _ => OnAttack?.Invoke();
+            _kickAction.performed += _ => OnKick?.Invoke();
         }
 
         public void Tick()
