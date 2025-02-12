4025a75d2c211df4df0c17cc6f5a79a167aa437f
Areg <areg06.06@mail.ru>
2025-02-13 00:23:34 +0300

    remove unused field in IWarrior

--- Assets/Scripts/BaseEntity/States/WarriorAttackState.cs
+++ Assets/Scripts/BaseEntity/States/WarriorAttackState.cs
@@ -2,10 +2,8 @@
 using System.Collections;
 using Agent;
 using Controllers.SoundManager;
-using EntityBehaviour;
 using UnityEngine;
 using VContainer;
-using Random = UnityEngine.Random;
 
 namespace BaseEntity.States
 {
@@ -27,7 +25,7 @@ namespace BaseEntity.States
             _warrior = Entity.GetComponent<IWarrior>();
             _warrior.OnAttack += Attack;
             _checkTargetsCoroutine = CheckTargets();
-            _warrior.Controller.StartCoroutine(_checkTargetsCoroutine);
+            Entity.StartCoroutine(_checkTargetsCoroutine);
         }
 
         public override void Update()
@@ -38,7 +36,7 @@ namespace BaseEntity.States
                     return;
             }
 
-            float distanceToTarget = Vector2.Distance(_target.transform.position, _warrior.Controller.transform.position);
+            float distanceToTarget = Vector2.Distance(_target.transform.position, Entity.transform.position);
             if (distanceToTarget <= _warrior.AttackRadius)
             {
                 if (!_isAttack)
@@ -55,8 +53,8 @@ namespace BaseEntity.States
             _isAttack = false;
             _target = null;
             _physicAgent.Stop();
-            if (_warrior.Controller != null)
-                _warrior.Controller.StopCoroutine(_checkTargetsCoroutine);
+            if (Entity != null)
+                Entity.StopCoroutine(_checkTargetsCoroutine);
             _checkTargetsCoroutine = null;
         }
         
@@ -71,7 +69,7 @@ namespace BaseEntity.States
 
         private bool TryFindEnemyInLookRadius()
         {
-            _target = _warrior.Controller.TargetFinder.FindObjectInLookRadius(_warrior.Controller.transform.position)
+            _target = Entity.TargetFinder.FindObjectInLookRadius(Entity.transform.position)
                 ?.GetComponent<BaseEntityController>();
             if (!_target) OnComplete?.Invoke();
             return _target;
@@ -82,8 +80,8 @@ namespace BaseEntity.States
             if(!_target) return;
             if (_isAttack)
             {
-                Vector2 dir = _target.transform.position - _warrior.Controller.transform.position;
-                _warrior.Controller.Rotate(dir.normalized, Time.fixedDeltaTime);
+                Vector2 dir = _target.transform.position - Entity.transform.position;
+                Entity.Rotate(dir.normalized, Time.fixedDeltaTime);
                 _physicAgent.Stop();
             }
             else
@@ -93,14 +91,14 @@ namespace BaseEntity.States
         private void StartAttackAnim()
         {
             _soundManager.StartMusic("WarriorAttack", SoundChannel.Effect);
-            _warrior.Controller.Animator.SetTrigger(_attack);
+            Entity.Animator.SetTrigger(_attack);
         }
 
         public void Attack()
         {
             _isAttack = false;
             if (_target is null) return;
-            if (_target.GetDistanceBetweenEntities(_warrior.Controller) > _warrior.AttackRadius)
+            if (_target.GetDistanceBetweenEntities(Entity) > _warrior.AttackRadius)
                 return;
             if (_target.Damage(_warrior.AttackDamage))
             {
@@ -113,7 +111,6 @@ namespace BaseEntity.States
     public interface IWarrior
     {
         public Action OnAttack { get; set; }
-        public EntityController Controller { get; }
         public int AttackDamage { get; }
         public float AttackRadius { get; }
     }
