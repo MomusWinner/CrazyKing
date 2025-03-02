e3892a5792098ffe7a9c39ecf5e4a34227322345
Areg <areg06.06@mail.ru>
2025-03-03 00:21:36 +0300

    add rope

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private Transform _target;
+
+    public void Update()
+    {
+        _lineRenderer.SetPosition(1, _target.position - transform.position );
+    }
+}
