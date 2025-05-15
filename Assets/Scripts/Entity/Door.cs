40dcf2cc1e6c3e048640155893467a3aad8f6ca2
Areg <areg06.06@mail.ru>
2025-05-15 23:18:38 +0300

    Implement Key and Door

---
+++ Assets/Scripts/Entity/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Entity
+{
+    public class Door : MonoBehaviour
+    {
+        [SerializeField] private Transform _leftSide;
+        [SerializeField] private Transform _rightSide;
+        [SerializeField] private Key _key;
+        private bool _isOpening;
+
+        private void Update() {
+            if (_isOpening) return;
+            
+            if ((_key.transform.position - transform.position).sqrMagnitude < 15f)
+            {
+                _isOpening = true;
+                StartCoroutine(WaitKey());
+            }
+        }
+
+        public void Open()
+        {
+            _leftSide.DORotate(new Vector3(0, 0, 110), 1f).SetEase(Ease.OutBounce);
+            _rightSide.DORotate(new Vector3(0, 0, -110), 1f).SetEase(Ease.OutBounce);
+        }
+
+        private IEnumerator WaitKey()
+        {
+            yield return _key.MoveToDoor(transform.position);
+            Open();
+            Destroy(_key.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Entity/Key.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using Entity.King;
+using UnityEngine;
+
+namespace Entity
+{
+    public class Key : MonoBehaviour
+    {
+        public bool IsTaken { get; private set; }
+        [SerializeField] private float _followingRadius;
+        [SerializeField] private float _speed;
+        
+        private Transform _kingTransform;
+        private bool _isMovingToDoor;
+
+        private void Update()
+        {
+            if (_kingTransform == null || _isMovingToDoor) 
+            {
+                return;
+            }
+
+            float distance = (_kingTransform.position - transform.position).magnitude;
+            if (distance < _followingRadius)
+            {
+                return;
+            }
+
+            transform.position = Vector3.Lerp(
+                transform.position,
+                _kingTransform.position,
+                _speed * Time.deltaTime);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (LayerMasks.King != 1 << other.gameObject.layer)
+            {
+                return;
+            }
+
+            if (!other.TryGetComponent(out KingController king))
+            {
+                return;
+            }
+            
+            _kingTransform = other.transform;
+            IsTaken = true;
+        }
+
+        public IEnumerator MoveToDoor(Vector3 doorPosition)
+        {
+            _isMovingToDoor = true;
+            while (true)
+            {
+                if ((doorPosition - transform.position).sqrMagnitude < 0.01f)
+                {
+                    break;
+                }
+                transform.position = Vector3.Lerp(
+                    transform.position,
+                    doorPosition,
+                    _speed * 2 * Time.deltaTime);
+                yield return null;
+            }
+        }
+    }
+}
